be6dea1de9e310b3fa1cf2259a3f9fbfd8914b71
rukny <ZINC@Zains-MacBook-Pro.local>
2016-07-02 14:11:01 +0300

    my first commit

---
+++ app/views/videos/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @video, :id, :description, :wistiaid, :created_at, :updated_at
---
+++ db/migrate/20160702103240_add_title_to_videos.rb
@@ -0,0 +1,6 @@
+class AddTitleToVideos < ActiveRecord::Migration
+  def change
+    add_column :videos, :title, :string
+  end
+end
+	
